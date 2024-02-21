f2c81c3a2a064eff835cd4598828af26e596dea2
Andriele Mendonça <dielemendonca@hotmail.com>
2024-02-20 21:40:52 -0300

    to do list added

---
+++ src/App.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+FALTA:
+
+1. Possibilitar que o consultar( ) retorne todos, só marcados ou só não marcados.
+2. Método filtrar() por categorias.
+
+ */
+public class App {
+    static ArrayList<Tarefa> tarefas = new ArrayList<>();
+
+
+
+    public static void adicionar(Scanner novaTarefa) {
+        System.out.print("Título da tarefa: ");
+        String titulo = novaTarefa.nextLine();
+
+        System.out.print("Descrição: ");
+        String descricao = novaTarefa.nextLine();
+
+        System.out.println("Digite o NÚMERO da categoria:\n" + "1 - Trabalho;\n" + "2 - Casa;\n" + "3 - Saúde e bem estar;\n" + "4 - Estudos;\n" + "5 - Religião;\n" + "6 - Lazer;\n" + "7 - Outros.");
+        int categoria = novaTarefa.nextInt();
+
+        Tarefa t1 = new Tarefa(titulo, descricao, categoria);
+        tarefas.add(t1);
+    }
+
+    public static void remover(Tarefa t1) {
+        tarefas.remove(t1);
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner novaTarefa = new Scanner(System.in);
+        int cont = 1;
+        ArrayList<Integer> numerosTarefas = new ArrayList<>();
+
+        while (true) {
+            System.out.println("TAREFA "+cont+": ");
+
+            adicionar(novaTarefa);
+            numerosTarefas.add(cont);
+
+            System.out.print("Deseja adicionar outra tarefa? (s/n): ");
+            String resposta = novaTarefa.next();
+
+            if (!resposta.equalsIgnoreCase("s")) {
+                break; // Sai do loop se a resposta não for "s"
+            }
+
+            cont +=1;
+            novaTarefa.nextLine(); // Limpa o buffer do scanner
+        }
+
+        //t1.marcar();
+        //t1.desmarcar();
+
+        for (int i = 0; i < tarefas.size(); i++){
+            System.out.println("TAREFA " + numerosTarefas.get(i) + "\n");
+            System.out.println(tarefas.get(i).consultar()+"\n");
+        }
+    }
+}
---
+++ src/Tarefa.java
@@ -0,0 +1,70 @@
+public class Tarefa {
+    public String titulo;
+    public String descricao;
+    public int categoria;
+    public boolean status = false;
+
+    /*
+    Categorias: 1 - Trabalho; 2 - Casa; 3 - Saúde e bem estar;
+    4 - Estudos; 5 - Religião; 6 - Lazer; 7 - Outros.
+    */
+
+    public Tarefa(String titulo, String descricao, int categoria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.categoria = categoria;
+    }
+
+    public void marcar() {
+        this.status = true;
+        System.out.println("Parabéns! Tarefa finalizada!");
+    }
+
+    public void desmarcar() {
+        this.status = false;
+        System.out.println("Tarefa pendente.");
+    }
+
+    public String getStatus() {
+        if (status) {
+            return "Finalizada";
+        } else {
+            return "Pendente";
+        }
+    }
+
+    public String getCategoria() {
+        switch (categoria) {
+            case 1:
+                return "Trabalho";
+            case 2:
+                return "Casa";
+            case 3:
+                return "Saúde e Bem Estar";
+            case 4:
+                return "Estudos";
+            case 5:
+                return "Religião";
+            case 6:
+                return "Lazer";
+            default:
+                return "Outros";
+        }
+    }
+
+    public String consultar() {
+        return "Título: " + titulo + "\nStatus: " + getStatus() + "\nCategoria: " + getCategoria() + "\nDescrição: " + descricao;
+    }
+
+    /*
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    } */
+}
+
+
+
